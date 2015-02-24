4ce2fe87d6ef27248ce0876365373211634db245
Yura Trambitskiy <tr.yura@yahoo.com>
2015-02-24 20:50:50 +0300

    drew basic gauge

--- src/gauge.js
+++ src/gauge.js
@@ -1,4 +1,6 @@
-var gauge = function(el, data,  options) {
+var gauge = function(el, data, options) {
+
+    if (!data) return;
 
     if (el.length) {
         for (var i; i<el.length; i++) {
@@ -10,65 +12,72 @@ var gauge = function(el, data,  options) {
     var defaults = {
         aperture: 270
     };
-
     options = gauge.extend(defaults, options);
+
+    var segmentsLength = data.marks.length-1;
+
+    for (var j=0; j<segmentsLength; j++) {
+        var div = gauge.getSegment(j, segmentsLength, el.clientWidth, options.aperture);
+        el.appendChild(div);
+    }
 
 };
 gauge.extend = function (target, source) {
     target = target || {};
     for (var prop in source) {
         if (typeof source[prop] === 'object') {
-            target[prop] = yaPresentation.extend(target[prop], source[prop]);
+            target[prop] = gauge.extend(target[prop], source[prop]);
         } else {
             target[prop] = source[prop];
         }
     }
     return target;
 };
+gauge.vendors = [
+    "-webkit-",
+    "-moz-",
+    "-ms-",
+    "-o-",
+    ""
+];
 gauge.extend(gauge, {
-    inherit: function(proto) {
-        function F() {}
-        F.prototype = proto;
-        var object = new F;
-        return object;
-    },
-    vendors: [
-        "-webkit-",
-        "-moz-",
-        "-ms-",
-        "-o-"
-    ],
-    element: {
-        draw: function() {
-            this.create();
-            this.position();
-        },
-        create: gauge.abstractMethod,
-        position: gauge.abstractMethod
-    },
-    draw: function(elements) {
-      for (var i; i<elements.length; i++) {
-          elements[i].draw();
-      }
-    },
-    abstractMethod: function() {
-        throw new Error("This method should be redefined");
-    },
-    Segment: function(index, total, className) {
+    getSegment: function(index, total, width, apertura) {
 
+        var outerDiv = document.createElement("div");
+        var innerDiv = document.createElement("div");
+        var circle = document.createElement("div");
+        circle.className = "gauge-circle";
 
+        outerDiv.style.position = "absolute";
+        outerDiv.style.top = 0;
+        outerDiv.style.left = 0;
+        outerDiv.style.overflow = "hidden";
+        innerDiv.style.overflow = "hidden";
+        outerDiv.style.width = width + "px";
+        innerDiv.style.width = width + "px";
+        circle.style.width = width + "px";
+        outerDiv.style.height = width/2 + "px";
+        innerDiv.style.height = width/2 + "px";
+        circle.style.height = width + "px";
+
+        var startConst = (360-apertura)/2;
+        var singleSize = (apertura/total+1);
+        var outerRotation = -90 + startConst + singleSize*index;
+        var innerRotation = -90 - (90 - singleSize);
+
+        for (var i=0; i<gauge.vendors.length; i++) {
+            outerDiv.style[gauge.vendors[i]+"transform-origin"] = "50% 100%";
+            innerDiv.style[gauge.vendors[i]+"transform-origin"] = "50% 100%";
+            outerDiv.style[gauge.vendors[i]+"transform"] = "rotate(" + outerRotation + "deg)";
+            innerDiv.style[gauge.vendors[i]+"transform"] = "rotate(" + innerRotation + "deg)";
+            circle.style[gauge.vendors[i]+"border-radius"] = "50%";
+            circle.style[gauge.vendors[i]+"box-sizing"] = "border-box";
+        }
+
+        outerDiv.appendChild(innerDiv);
+        innerDiv.appendChild(circle);
+
+        return outerDiv;
 
     }
 });
-gauge.dot = gauge.inherit(gauge.element);
-gauge.label = gauge.inherit(gauge.element);
-gauge.arrow = gauge.inherit(gauge.element);
-gauge.segment = gauge.inherit(gauge.element);
-gauge.segment.draw = function() {
-    var outerDiv = document.createElement("div");
-    var innerDiv = document.createElement("div");
-};
-gauge.Dot.prototype = gauge.dot;
-gauge.Label.prototype = gauge.label;
-gauge.Arrow.prototype = gauge.arrow;
-gauge.Segment.prototype = gauge.segment;
